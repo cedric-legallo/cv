315ee736bd1edfaa4a0b1b658f9d8ebfdd5c2d58
Cedric Le Gallo <cedric.legallo+github@gmail.com>
2013-06-17 10:48:24 +0200

    syntaxe et timer

--- public/js/cv.js
+++ public/js/cv.js
@@ -1,5 +1,5 @@
 setTimeout(function(){
-	var t = $(".slider-item");
+        var t = document.querySelectorAll(".slider-item");
 	for (var i = 0; i < t.length; i++){t[i].parentNode.removeChild(t[i]);}
 	for (var i = t.length-1; i >=0 ; i--){$(".slider-item-container")[0].appendChild(t[i]);}
-});
+}, 2500);
